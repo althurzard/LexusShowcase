aab085fbd63c31d570587ade9f9f59723216384e
mac <nguyen_quocvuong@hotmail.com>
2017-07-11 15:15:56 +0700

    implement header

--- LexusApp/CustomHeader.swift
+++ LexusApp/CustomHeader.swift
@@ -18,9 +18,65 @@ class CustomHeader: UIView {
     }
     */
     
+    @IBOutlet weak var tongQuanView: UIView!
+
+    @IBOutlet weak var thietKeView: UIView!
+    
+    
+    @IBOutlet weak var vanHanhView: UIView!
+    
+    @IBOutlet weak var anToanView: UIView!
+    
+    @IBOutlet weak var thongSoView: UIView!
+    
+    @IBOutlet weak var thuVienView: UIView!
+    
+    
+    @IBOutlet weak var underlineView: UIView!
+    
+    
+    enum LabelName: Int {
+        case TongQuan
+        case ThietKe
+        case VanHanh
+        case AnToan
+        case ThongSo
+        case ThuVien
+    }
     
     class func instanceFromNib() -> UIView {
         return UINib(nibName: "CustomHeader", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! UIView
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        tongQuanView.addGestureRecognizer(createTap())
+        thietKeView.addGestureRecognizer(createTap())
+        vanHanhView.addGestureRecognizer(createTap())
+        anToanView.addGestureRecognizer(createTap())
+        thongSoView.addGestureRecognizer(createTap())
+        thuVienView.addGestureRecognizer(createTap())
+    }
+    
+    func createTap() -> UITapGestureRecognizer {
+        return UITapGestureRecognizer(target: self, action: #selector(didTapOnLabel))
+    }
+    
+    func didTapOnLabel(sender:UITapGestureRecognizer) {
+
+        if let view = sender.view  {
+            let lblName = LabelName(rawValue: view.tag)!
+            print(lblName.rawValue)
+//            switch lblName {
+//            case .TongQuan:
+//                
+//            case .ThietKe:
+//            case .VanHanh:
+//            case .AnToan:
+//            case .ThongSo:
+//            case .ThuVien:
+//            }
+        }
     }
 
 }
